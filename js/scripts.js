50bf4dc443dd6897931eb0af17af52c0433b9043
Josh <Joshua.horgen@gmail.com>
2019-06-14 16:23:17 -0700

    More experimentation

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,26 @@ $(document).ready(function(){
     // console.log("yes");
 
 
-  });
+    var outputArray = [];
+    var beep = ($("#input-num").val()).split("");
+    outputArray.push(beep);
+    console.log(beep);
+    for (var i=0; i < beep.length; i++) {
+      if (beep[i] === "1") {
+          beep[i] = "Beep";
+          console.log(outputArray);
+        } else if (beep[i] === "2") {
+           beep[i] = "Boop";
+        } else if (beep[i] === "3") {
+          beep[i] = "Sorry dave, I'm afraid I can't do that.";
+        } else if (beep[i] === "13") {
+          beep[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+        } else if (beep[i] === "21") {
+          beep[i] = "Boop";
+        } else if (beep[i] === "32") {
+          beep[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+        }
+};
+$("#output").text(outputArray);
 });
-
-var ouputArray = [];
-var beep = $("#input-num").val();
-  for (var i = 0; i < beep; i++) {
-    beep = [];
-    console.log(beep);
-    console.log(beep);
-    console.log(outputArray);
-    console.log(beep);
-
-  };
-  // $(beep).push(outputArray);
-  // $("#output").push(outputArray);
-// });
+});
