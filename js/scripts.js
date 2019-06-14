f1448c48fe9508ce7662420dce10b6ece88323bc
Josh <Joshua.horgen@gmail.com>
2019-06-14 12:11:16 -0700

    Try new solutions to loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,13 @@ $(document).ready(function(){
     event.preventDefault()
     var beep = $("#input-num").val();
 
+    var ouputArray = [];
+    var inputArray = ['1', '2', '3', '4', '5'];
+    for (var inputArray = 0; i < inputArray.length; inputArray++) {
+      console.log(beep);
+      outputArray.push(beep);
+      console.log(outputArray);
+      console.log(inputArray);
+    };
   });
 });
